5f35938f2791f65d02883f94739fde9427132b08
Alberto97 <albertop2197@gmail.com>
2018-06-30 18:01:36 +0200

    griffin: Actions: Allow enabling Hand wave when doze on Pick Up is the only enabled option in AmbientDisplayConfiguration
    Change-Id: I4ddc322e6cf2c0ea2361952c54419d00eb93537d

--- LineageActions/src/org/lineageos/settings/device/LineageActionsSettings.java
+++ LineageActions/src/org/lineageos/settings/device/LineageActionsSettings.java
@@ -17,10 +17,12 @@
 
 package org.lineageos.settings.device;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.UserHandle;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 
 import com.android.internal.hardware.AmbientDisplayConfiguration;
 
@@ -76,7 +78,7 @@ public class LineageActionsSettings {
     }
 
     public boolean isIrWakeupEnabled() {
-        return isDozeEnabled() && !isAODEnabled() && mIrWakeUpEnabled;
+        return (isPulseOnPickupEnabled() || isDozeEnabled()) && !isAODEnabled() && mIrWakeUpEnabled;
     }
 
     public boolean isIrSilencerEnabled() {
@@ -89,6 +91,12 @@ public class LineageActionsSettings {
 
     public boolean isLiftToSilenceEnabled() {
         return mLiftToSilenceEnabled;
+    }
+
+    public boolean isPulseOnPickupEnabled() {
+        return Settings.Secure.getIntForUser(mContext.getContentResolver(),
+            Settings.Secure.DOZE_PULSE_ON_PICK_UP, 1, UserHandle.USER_CURRENT) != 0
+                && mAmbientDisplayConfiguration.pulseOnPickupAvailable();
     }
 
     public void cameraAction() {
