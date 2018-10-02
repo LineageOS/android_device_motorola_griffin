6545f30ef920b2d7370535a7bff05e13942da286
Han Wang <416810799@qq.com>
2018-10-02 14:00:56 +0200

    griffin: Update textclassifier package name for P
    Change-Id: If846cc590060025202fa336af4420747f6b01238

--- device.mk
+++ device.mk
@@ -371,7 +371,7 @@ PRODUCT_PACKAGES += \
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
-    textclassifier.smartselection.bundle1
+    textclassifier.bundle1
 
 # USB
 PRODUCT_PACKAGES += \
